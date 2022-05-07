bd4a5c85d861f28858507731cfb9ad2b3b5106bf
Posokhova Natalya <natalya.posokhova@gmail.com>
2022-05-07 12:27:36 +0500

    Override Equals for unit tests.

--- TaskMonitoring.Cards.BL.Interface/DTO/Task.cs
+++ TaskMonitoring.Cards.BL.Interface/DTO/Task.cs
@@ -13,5 +13,19 @@ namespace TaskMonitoring.Cards.BL.Interface.DTO
 		public int Id { get; set; }
 
 		public IEnumerable<string> Comments { get; set; }
+
+		public override bool Equals(object obj)
+		{
+			//Check for null and compare run-time types.
+			if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+			{
+				return false;
+			}
+			else
+			{
+				Task task = (Task)obj;
+				return (Id == task.Id) && (Title == task.Title) && (Summary == task.Summary);
+			}
+		}
 	}
 }
