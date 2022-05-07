61a34824df2180302835c90c16a0700e446fcceb
Posokhova Natalya <natalya.posokhova@gmail.com>
2022-05-07 12:51:52 +0500

    Add Data Access project.

---
+++ TaskMonitoring.Cards.DataAccess/DataAccess.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using TaskMonitoring.Cards.BL.Interface.DTO;
+using TaskMonitoring.Cards.DataAccess.Interface;
+
+namespace TaskMonitoring.Cards.DataAccess
+{
+	public class DataAccess : IDataAccess
+	{
+		public void AddComment(long taskId, string comment)
+		{
+			throw new NotImplementedException();
+		}
+
+		public long AddTask(long userId, Task task)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void DeleteTask(long taskId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<Task> GetAllTasksByUserId(long userId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void UpdateTask(Task task)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
