2ab81619f4c92521da32b088c843d258db84686d
Posokhova Natalya <natalya.posokhova@gmail.com>
2022-05-07 13:24:08 +0500

    Add create task realization.

--- TaskMonitoring.Cards.BL/TaskService.cs
+++ TaskMonitoring.Cards.BL/TaskService.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using TaskMonitoring.Cards.BL.Interface.DTO;
 using TaskMonitoring.Cards.BL.Interface;
+using TaskMonitoring.Cards.DataAccess.Interface;
 
 namespace TaskMonitoring.Cards.BL
 {
 	public class TaskService : ITaskService
 	{
+		private IDataAccess _data;
+
 		public void AddComment(long taskId, string comment)
 		{
 			throw new NotImplementedException();
@@ -14,7 +17,8 @@ namespace TaskMonitoring.Cards.BL
 
 		public Task CreateTask(long userId, Task task)
 		{
-			throw new NotImplementedException();
+			var taskId = _data.AddTask(userId, task);
+			return _data.GetTaskById(taskId);
 		}
 
 		public void DeleteTaskById(long taskId)
