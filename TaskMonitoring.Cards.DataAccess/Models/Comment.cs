386c5eb31e7dc1c63407e9bef27268401dcec588
Posokhova Natalya <natalya.posokhova@gmail.com>
2022-06-10 21:06:58 +0500

    Add Comments

---
+++ TaskMonitoring.Cards.DataAccess/Models/Comment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskMonitoring.Cards.DataAccess.Models
+{
+	public class Comment
+	{
+		public long Id { get; set; }
+		public string Content { get; set; }
+		public long TaskId { get; set; }
+	}
+}
---
+++ TaskMonitoring.Cards.DataAccess/Models/Task.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskMonitoring.Cards.DataAccess.Models
+{
+	public class Task
+	{
+		public long Id { get; set; }
+		public string Title { get; set; }
+		public string Summary { get; set; }
+		public IEnumerable<Comment> Comments { get; set; }
+	}
+}
