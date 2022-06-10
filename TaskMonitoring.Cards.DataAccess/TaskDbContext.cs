d6509cf5e92f1416f0efa2a8ce3061ccc72a5cb4
Posokhova Natalya <natalya.posokhova@gmail.com>
2022-06-10 22:07:31 +0500

    Create context

---
+++ TaskMonitoring.Cards.DataAccess/TaskDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskMonitoring.Cards.DataAccess.Models;
+
+namespace TaskMonitoring.Cards.DataAccess
+{
+	public class TaskDbContext : DbContext
+	{
+		public DbSet<Task> Tasks => Set<Task>();
+		public DbSet<Comment> Comments => Set<Comment>();
+
+		public TaskDbContext() => Database.EnsureCreated();
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			if (!optionsBuilder.IsConfigured)
+				optionsBuilder.UseNpgsql("Data Source=task.db");
+		}
+	}
+}
