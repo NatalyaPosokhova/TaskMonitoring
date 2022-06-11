42d72c7d850df7ffa940a5c31fdf8fd4bafb9b55
Posokhova Natalya <natalya.posokhova@gmail.com>
2022-06-11 18:49:16 +0500

    Add user table

--- TaskMonitoring.Cards.DataAccess/TaskDbContext.cs
+++ TaskMonitoring.Cards.DataAccess/TaskDbContext.cs
@@ -5,9 +5,14 @@ namespace TaskMonitoring.Cards.DataAccess
 {
 	public class TaskDbContext : DbContext
 	{
+		public DbSet<User> Users => Set<User>();
 		public DbSet<Task> Tasks => Set<Task>();
 		public DbSet<Comment> Comments => Set<Comment>();
 		public TaskDbContext(DbContextOptions<TaskDbContext> options) : base(options)
+		{
+		}
+
+		public TaskDbContext()
 		{
 		}
 
