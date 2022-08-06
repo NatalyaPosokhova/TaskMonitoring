cd7d0233d0e3c8f22053a575bdfb1eea710ff263
Posokhova Natalya <natalya.posokhova@gmail.com>
2022-08-06 21:19:32 +0500

    +

--- TaskMonitoring.Cards.DataAccess/TaskDbContext.cs
+++ TaskMonitoring.Cards.DataAccess/TaskDbContext.cs
@@ -10,6 +10,7 @@ namespace TaskMonitoring.Cards.DataAccess
 		public DbSet<Comment> Comments => Set<Comment>();
 		public TaskDbContext(DbContextOptions<TaskDbContext> options) : base(options)
 		{
+			Database.EnsureCreated();
 		}
 
 		//public TaskDbContext() => Database.EnsureCreated();
