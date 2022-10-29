59e0306c60120e1f20f445637eeddade882be232
Posokhova Natalya <natalya.posokhova@gmail.com>
2022-10-29 13:53:54 +0500

    Introduce web proxy

---
+++ TaskMonitoring.APIClients/WebProxy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskMonitoring.APIClients
+{
+	public class WebProxy<TRequest, TResponse>
+	{
+		public async Task<TResponse> Get(string query, TRequest request)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
