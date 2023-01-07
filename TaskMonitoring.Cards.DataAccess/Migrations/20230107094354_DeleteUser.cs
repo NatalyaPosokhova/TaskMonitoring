f336c2fc26c147f5a9df7e5ca6eb6ea5bfeae422
Posokhova Natalya <natalya.posokhova@gmail.com>
2023-01-07 15:16:08 +0500

    Fix problem with migration

---
+++ TaskMonitoring.Cards.DataAccess/Migrations/20230107094354_DeleteUser.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace TaskMonitoring.Cards.DataAccess.Migrations
+{
+    public partial class DeleteUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_Users_UserId",
+                table: "Tasks");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tasks_UserId",
+                table: "Tasks");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "UserId",
+                table: "Tasks",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "UserId",
+                table: "Tasks",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tasks_UserId",
+                table: "Tasks",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_Users_UserId",
+                table: "Tasks",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+    }
+}
--- TaskMonitoring.Cards.DataAccess/TaskDbContext.cs
+++ TaskMonitoring.Cards.DataAccess/TaskDbContext.cs
@@ -7,12 +7,25 @@ namespace TaskMonitoring.Cards.DataAccess
 	{
 		public DbSet<Task> Tasks => Set<Task>();
 		public DbSet<Comment> Comments => Set<Comment>();
+
+#if _FOR_MIGRATE_
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			base.OnConfiguring(optionsBuilder);
+			optionsBuilder.UseNpgsql();
+		}
+
+		public TaskDbContext()
+		{
+
+			Database.SetConnectionString("Server=localhost;Port=5432;Database=taskdb;User ID=postgres;Password=qwerty");
+			Database.EnsureCreated();
+		}
+#else
 		public TaskDbContext(DbContextOptions<TaskDbContext> options) : base(options)
 		{
 			Database.EnsureCreated();
 		}
-
-		//public TaskDbContext() => Database.EnsureCreated();
-
+#endif
 	}
 }
