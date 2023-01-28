dd444b1a3b8480a6ec8b476f870f371047a56212
Posokhova Natalya <natalya.posokhova@gmail.com>
2023-01-28 14:31:20 +0500

    Add empty projects

---
+++ Time microservice/TaskMonitoring.Timing/Controllers/TimingController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskMonitoring.Timing.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class TimingController : ControllerBase
+	{
+	}
+}
