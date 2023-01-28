d904f38bb13a0f04e3f89ece987672c3f59cad48
Posokhova Natalya <natalya.posokhova@gmail.com>
2023-01-28 15:00:16 +0500

    Add timing BL interface

---
+++ Time microservice/TaskMonitoring.Timing.BL.Interface/ITimingService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskMonitoring.Timing.BL.Interface
+{
+	public interface ITimingService
+	{
+		public void LaunchTimer(long userId, long taskId);
+		public TimeSpan StopTimer(long userId, long taskId);
+		public IDictionary<long, IEnumerable<TimeSpan>> GetReport(long userId, IEnumerable<long> tasks);
+	}
+}
