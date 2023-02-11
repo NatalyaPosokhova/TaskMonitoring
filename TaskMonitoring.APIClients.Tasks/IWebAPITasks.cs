2db7bc2f69dc65d49bbd09aa68e399380544071e
Posokhova Natalya <natalya.posokhova@gmail.com>
2023-02-11 13:55:20 +0500

    add task proxy interface.

--- TaskMonitoring.APIClients.Tasks/IWebAPITasks.cs
+++ TaskMonitoring.APIClients.Tasks/IWebAPITasks.cs
@@ -1,7 +1,15 @@
-﻿namespace TaskMonitoring.APIClients.Tasks
+﻿using TaskMonitoring.APIClients.Tasks.DTO;
+using TaskMonitoring.Cards.Data;
+
+namespace TaskMonitoring.APIClients.Tasks
 {
 	public interface IWebAPITasks
 	{
-
+		public Task<TaskProxyDTO> CreateTask(long userId, NewTaskCard task);
+		public Task DeleteTaskById(long userId, long taskId);
+		public Task AddComment(long userId, long taskId, string comment);
+		public Task<IEnumerable<TaskProxyDTO>> GetAllTasks(long userId);
+		public Task UpdateTask(long userId, NewTaskCard task);
+		public Task<TaskProxyDTO> GetTaskById(long userId, long taskId);
 	}
 }
