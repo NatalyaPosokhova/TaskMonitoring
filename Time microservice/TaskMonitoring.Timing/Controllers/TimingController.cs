ca013eed2332c7afed27c99e96838aaecb23cbb4
Posokhova Natalya <natalya.posokhova@gmail.com>
2023-02-26 13:54:05 +0500

    Add not filled timing controller

--- Time microservice/TaskMonitoring.Timing/Controllers/TimingController.cs
+++ Time microservice/TaskMonitoring.Timing/Controllers/TimingController.cs
@@ -1,11 +1,35 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using TaskMonitoring.Timing.BL.Interface;
 
 namespace TaskMonitoring.Timing.Controllers
 {
-	[Route("api/[controller]")]
+	[Route("[controller]/[action]")]
 	[ApiController]
 	public class TimingController : ControllerBase
 	{
+		private readonly ITimingService _timingService;
+		public TimingController(ITimingService timingService)
+		{
+			_timingService = timingService;
+		}
+
+		[HttpPost]
+		public void LaunchTimer(long userId, long taskId, DateTime startTime)
+		{
+			throw new NotImplementedException();
+		}
+
+		[HttpPost]
+		public TimeSpan StopTimer(long userId, long taskId)
+		{
+			throw new NotImplementedException();
+		}
+
+		[HttpPost]
+		public IDictionary<long, IEnumerable<TimeSpan>> GetReport(long userId, IEnumerable<long> tasks)
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
